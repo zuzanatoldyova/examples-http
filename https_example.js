15edbf800ac7b7d2aff6b38840949518980c8dcc
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-12 01:09:40 +0000

    modified host and path for monthy python script data

--- https_example.js
+++ https_example.js
@@ -3,8 +3,8 @@ var https = require('https');
 console.log('I did it!');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
