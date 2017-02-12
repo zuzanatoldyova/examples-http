e4a1b9b1ad11fb83dd6d17342ae2c36fd68c96cd
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-12 01:07:24 +0000

    modified callback to show chunk of data

--- https_example.js
+++ https_example.js
@@ -10,8 +10,11 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log(response);
 
+  response.on('data', function(chunk) {
+  console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+  console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
